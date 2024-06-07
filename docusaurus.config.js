ea885418d049b2777925a00118b5b1bef4d1634b
ryanxin7 <xinxincn0506@outlook.com>
2024-06-07 17:29:18 +0800

    github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,19 +9,19 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'wwwww',
+  title: 'Ryan\'s Wiki',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ryanxin7.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/xwiki/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ryanxin7', // Usually your GitHub org/user name.
+  projectName: 'xwiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
