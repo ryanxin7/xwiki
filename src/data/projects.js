984e3dfcadfa6e0aa96f05cd7874cd295796adb5
ryanxin7 <xinxincn0506@outlook.com>
2024-08-18 21:36:12 +0800

    增加模块

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
     id: 1,
     title: 'Kubernetes',
     imageUrl: 'https://cdn1.ryanxin.live/xwiki/kubernetes.jpg',
-    description: 'Kubernetes 从略懂到精通😄',
+    description: 'Kubernetes 从入门到精通😄',
     link: '/k8s/',
   },
   {
@@ -27,6 +27,35 @@ const projects = [
     description: '搜索和分析、数据采集以及可视化🧐',
     link: '/elk/elk-Elasticsearch',
   },
+  {
+    id: 5,
+    title: 'Loki',
+    imageUrl: 'https://cdn1.ryanxin.live/loki1.png',
+    description: '由 Grafana Labs 开发的日志聚合系统',
+    link: '/Loki/elk-Elasticsearch',
+  },
+  {
+    id: 6,
+    title: 'Tekton',
+    imageUrl: 'https://cdn1.ryanxin.live/tekton.png',
+    description: '基于CRD的CI/CD系统',
+    link: '/Loki/elk-Elasticsearch',
+  },
+  {
+    id: 7,
+    title: 'Traefik',
+    imageUrl: 'https://cdn1.ryanxin.live/treafik.png',
+    description: '云原生反向代理和负载均衡器',
+    link: '/Loki/elk-Elasticsearch',
+  },
+  {
+    id: 8,
+    title: 'OpenKruise',
+    imageUrl: 'https://cdn1.ryanxin.live/Kruise.png',
+    description: '扩展和增强 Kubernetes 应用管理能力',
+    link: '/Loki/elk-Elasticsearch',
+  },
+
 
   // 继续添加更多项目
 ];
